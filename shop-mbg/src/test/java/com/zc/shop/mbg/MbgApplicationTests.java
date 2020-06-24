9dd16b5164b465a25952d81505874644da6d32d4
zc <584787434@qq.com>
2020-06-24 17:39:15 +0800

    仿mall项目结构

---
+++ shop-mbg/src/test/java/com/zc/shop/mbg/MbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc.shop.mbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
