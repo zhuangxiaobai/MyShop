f1e16e3af8f8e6158aec47334bd6d68c07cca9bf
zhuangxiaobai <584787434@qq.com>
2020-07-25 23:29:57 +0800

    ，

--- shop-admin/src/main/java/com/zc/shop/admin/service/impl/GoodsServiceImpl.java
+++ shop-admin/src/main/java/com/zc/shop/admin/service/impl/GoodsServiceImpl.java
@@ -16,9 +16,15 @@ public class GoodsServiceImpl implements GoodsService {
 
        //获取品种,品名,规格,材质 1.已经在attribute表中存在不管 2.不存在，添加
         Attribute  attribute = new Attribute();
-        //获取品种
-        attribute.setAttrName(goodsCreateParam.getAttrCatName());
-        //
+        //获取品种，品种是写死的
+         String pingZhong = goodsCreateParam.getAttrCatName();
+
+         //获取品名，规格，材质
+        attribute.setAttrName(goodsCreateParam.getAttrName());
+        attribute.setAttrName(goodsCreateParam.getSpecifications());
+        attribute.setAttrName(goodsCreateParam.getMaterial());
+
+        //去查询Attribute表看看存不存在品名，规格，材质相同的信息，不存在就添加，存在就不管
 
 
 
