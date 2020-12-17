188d2d4e7650546fd602fa9d9029e11c6b39a48a
庄小白 <584787434@qq.com>
2020-12-17 10:56:38 +0800

    添加了额外的注解异常验证

--- shop-common/src/main/java/com/zc/shop/common/exception/ExceptionControllerAdvice.java
+++ shop-common/src/main/java/com/zc/shop/common/exception/ExceptionControllerAdvice.java
@@ -5,7 +5,12 @@ import com.zc.shop.common.api.CommonResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.Set;
 
 /**
  * 全局异常处理
@@ -13,7 +18,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
-    //处理参数验证的异常
+    //处理(jdk提供的)参数验证的异常(@valid)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public CommonResult MethodArgumentNotValidException(MethodArgumentNotValidException e) {
        // CommonResult commonResult;
@@ -21,6 +26,24 @@ public class ExceptionControllerAdvice {
 
         return    CommonResult.validateFailed(objectError.getDefaultMessage());
     }
+    /**
+     * 处理(spring提供的)参数验证的异常(@valided)
+     * ConstraintViolationException 参数校验统一异常处理
+     */
+    @ResponseBody
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public CommonResult errorHandler(ConstraintViolationException ex) {
+        //logger.error(ex.getMessage(),ex);
+        Set<ConstraintViolation<?>> violations = ex.getConstraintViolations();
+//        for (ConstraintViolation<?> item : violations) {
+//            return CommonResult.validateFailed(item.getMessage());
+//        }
+
+        return CommonResult.validateFailed(violations.iterator().next().getMessage());
+    }
+
+
+
 
     //处理自定义业务异常
     @ExceptionHandler(BusinessException.class)
